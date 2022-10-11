9bfdaa1934e456738fdefa65e31806ffb549de03
npro1001 <nprocacciojr@gmail.com>
2022-10-11 19:42:02 -0400

    Goals controller and routes setup

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,20 @@
 // Entry point to server
-console.log("hello world");
+
+const express = require('express')
+const dotenv = require('dotenv').config() // allows us to have .env file with variables
+const { errorHandler } = require('./middleware/errorMiddleware')
+const port = process.env.PORT || 5001
+
+
+const app = express()
+
+// To send post data via url or body
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.use(errorHandler)
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
+
